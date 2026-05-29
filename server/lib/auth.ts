5498ffa7467edb81e2683c3409c464ae352ee586
ASC-07 <adityachauhan75103@gmail.com>
2026-05-29 12:07:45 +0530

    fix cross-domain cookie issue

--- server/lib/auth.ts
+++ server/lib/auth.ts
@@ -10,26 +10,23 @@ export const auth = betterAuth({
         provider: "postgresql", 
     }),
     emailAndPassword: { 
-    enabled: true, 
-  },
-  user: {
-    deleteUser: {enabled: true}
-  },
-  trustedOrigins,
-  baseURL: process.env.BETTER_AUTH_URL!,
-  secret: process.env.BETTER_AUTH_SECRET!,
-  advanced: {
-    cookies: {
-        session_token: {
-            name: 'auth_session',
-            attributes: {
-                httpOnly: true,
-                secure: process.env.NODE_ENV === 'production',
-                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-                path: '/',
-            }
+        enabled: true, 
+    },
+    user: {
+        deleteUser: {enabled: true}
+    },
+    trustedOrigins,
+    baseURL: process.env.BETTER_AUTH_URL!,
+    secret: process.env.BETTER_AUTH_SECRET!,
+    advanced: {
+        crossSubdomainCookies: {
+            enabled: false
+        },
+        defaultCookieAttributes: {
+            secure: true,
+            httpOnly: true,
+            sameSite: 'none',
+            partitioned: true,
         }
     }
-  }
-  
 });
